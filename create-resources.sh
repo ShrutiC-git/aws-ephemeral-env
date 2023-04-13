cb901ca1234e8fcc1b60dc1e296daefee652da99
ShrutiC-git <shrutichaturvedi16.sc@gmail.com>
2023-04-13 19:04:44 +0200

    readd resources

--- create-resources.sh
+++ create-resources.sh
@@ -26,14 +26,14 @@ aws \
  --role-name admin-role \
  --path / \
  --assume-role-policy-document file:./admin-policy.json
-# echo "Make S3 bucket"
-# aws \
-#   s3 mb s3://lambda-functions \
-#   --endpoint-url http://localhost:4566 
-# echo "Copy the lambda function to the S3 bucket"
-# aws \
-#   s3 cp lambdas.zip s3://lambda-functions \
-#   --endpoint-url http://localhost:4566 
+echo "Make S3 bucket"
+aws \
+  s3 mb s3://lambda-functions \
+  --endpoint-url http://localhost:4566 
+echo "Copy the lambda function to the S3 bucket"
+aws \
+  s3 cp lambdas.zip s3://lambda-functions \
+  --endpoint-url http://localhost:4566 
 
 # echo "Create the lambda exampleLambda"
 # aws \
